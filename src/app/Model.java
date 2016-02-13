10b9fa44c94657e0665574079b58d9eb3de520c3
Your Name <you@example.com>
2016-02-13 18:09:14 -0200

    src/app/Model.java

--- src/app/Model.java
+++ src/app/Model.java
@@ -8,19 +8,7 @@ public abstract class Model {
 
 	public Model()
 	{
-		String parts[] = getClass().getName().split("\\.");	
-		String name = parts[parts.length-1];
-		
-		File file = new File(name + ".data");
-		
-		if(!file.exists() && !file.isDirectory()) { 
-		    try {
-				file.createNewFile();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+
 	}
 	
 }
